ce4bbdb7939ef9c894339490dc1aeff946e1c3f5
KORAY KELAM <kkelam19@ku.edu.tr>
2025-12-24 17:31:11 +0100

    Add GitHub Pages deploy workflow - 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  site: "https://koraykelam.github.io",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
