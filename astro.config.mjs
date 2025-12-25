8909589e235799e2d87c72e79b201e3909b21091
KORAY KELAM <kkelam19@ku.edu.tr>
2025-12-25 21:17:58 +0100

    Add sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://koraykelam.github.io",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
